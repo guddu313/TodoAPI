b33ceac1e1202eadc88d277b6965c16b13bbcc29
Pravin Gareta <pravin.gareta@einfochips.com>
2017-09-01 17:07:25 +0530

    Added POST support

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-},{
-	id: 2,
-	description: 'Go to Market',
-	completed: false
-},{
-	id: 3,
-	description: 'Go to office',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req,res){
 	res.send('Todo API Root');
@@ -47,6 +39,19 @@ app.get('/todos/:id', function(req,res){
 		res.status(404).send();
 	}
 
+});
+
+app.post('/todos', function(req, res){
+	var body = req.body;
+	
+	//add id field
+	body.id = todoNextId++;
+	
+	//push the data into array
+	todos.push(body);
+	//console.log('description: ' + body.description);
+	
+	res.json(body);
 });
 
 app.listen(PORT, function(){
