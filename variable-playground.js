648ac914000f936cf3b5a59bc2afb37200d456ca
Pravin Gareta <pravin.gareta@einfochips.com>
2017-09-02 14:57:40 +0530

    variable playground

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+/* var person = {
+	name: 'Pravin',
+	age: 21
+};
+
+function updateperson(obj){
+	/*obj = {
+		name:'pravin',
+		age: 27
+	};
+	obj.age = 27;
+}
+
+updateperson(person);
+
+console.log(person); */
+
+//Array exampled
+var grades = [15,18];
+
+function addGrades(grades)
+{
+	grades.push(55);
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
