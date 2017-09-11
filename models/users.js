277a73557d2409573239076a3581dab77065a6db
Pravin Gareta <pravin.gareta@einfochips.com>
2017-09-11 17:12:42 +0530

    Added login route for authenticate

--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ var _ = require('underscore');
 
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('user', {
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -43,6 +43,32 @@ module.exports = function(sequelize, DataTypes) {
 					}
 				}
 			},
+			classMethods: {
+				authenticate: function(body){
+						return new Promise(function(resolve, reject){
+								if(typeof body.email !== 'string' || typeof body.password !== 'string')
+								{
+									return reject();
+								}
+								
+								user.findOne({
+									where: {
+										email: body.email
+									}
+								}).then(function(user){
+									if(!user || !bcrypt.compareSync(body.password,user.get('password_hash')))
+									{
+										return reject();
+									}
+									
+									return resolve(user);
+									
+								},function(e){
+									return reject();
+								});														
+						});														
+				}
+			},
 			instanceMethods: {
 				toPublicJSON: function(){
 					var json = this.toJSON();
@@ -50,4 +76,6 @@ module.exports = function(sequelize, DataTypes) {
 				}
 			}
 		});
+		
+		return user;
 }
